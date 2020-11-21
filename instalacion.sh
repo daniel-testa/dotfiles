064f664d4bb85ca33bcbf1bb212d5d2325bf17ab
daniel testa <50175901+daniel-testa@users.noreply.github.com>
2020-11-21 01:14:45 -0300

    Add files via upload

--- instalacion.sh
+++ instalacion.sh
@@ -4,69 +4,112 @@ set -e
 
 ############ instalar paquetes desde repos oficiles
 echo "... Instalando paquetes oficiales:"
+sleep 1
 
-sudo pacman -S --needed --noconfirm alacritty dmenu exa firefox-i18n-es-ar git gtop htop lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings man-db man-pages mlocate networkmanager nitrogen picom powerline powerline-vim rofi vim wget xmobar xmonad xmonad-contrib xorg-apps xorg-server xorg-xinit zathura zathura-pdf-mupdf zsh zsh-completions zsh-syntax-highlighting
+sudo pacman -Syyuu --needed --noconfirm alacritty dmenu exa  ttf-ubuntu-font-family \
+	firefox-i18n-es-ar git gtop htop lightdm lightdm-gtk-greeter \
+	lightdm-gtk-greeter-settings man-db man-pages mlocate networkmanager nitrogen \
+	picom powerline powerline-vim rofi vim wget xmobar xmonad xmonad-contrib \
+	xorg-apps xorg-server xorg-xinit zathura zathura-pdf-mupdf zsh zsh-completions \
+	zsh-syntax-highlighting
 
 ## descomentar la sig. linea si se instala en virtualbox SO invitado
-#sudo pacman -S --needed --noconfirm virtualbox-guest-utils
+sudo pacman -S --needed --noconfirm virtualbox-guest-utils
 
+echo "...paquetes oficiales instalados"
+sleep 1
 
 ############ instalar yay-bin desde AUR
 
-echo "... Instalando yay AUR helper: "
+echo "... Instalando yay-bin:"
 
-wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay-bin -O PKGBUILD
-makepkg
-sudo pacman -U yay-bin-10.1.0-1-x86_64.pkg.tar.zst
+sleep 1
 
+mkdir tmp_yay && \
+       	cd tmp_yay
+wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay-bin -O PKGBUILD && \
+	makepkg -si --noconfirm
+cd
+rm -rf tmp_yay
 
 ############ instalar paquetes desde AUR
 
 echo "... Instalando paquetes de AUR:"
+sleep 1
 
 yay -S exa nerd-fonts-iosevka nerd-fonts-mononoki nerd-fonts-noto-sans-regular-complete nerd-fonts-terminus ttf-font-awesome
 
 
+echo "...paquetes AUR instalados"
+sleep 1
+
+############# clonar  dotfiles  ##################
+
+echo "...clonando dotfiles desde github:"
+
+sleep 1
+
+git clone --bare https://github.com/daniel-testa/dotfiles.git $HOME/.dotfiles
+
+function gdfiles {
+	   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+   }
+
+gdfiles checkout
+
+if [ $?=0 ]
+then
+	echo "...checkout correcto:"
+else
+	echo "Backing up pre-existing dot files.";
+	mkdir -p backup.dotfiles
+	gdfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} backup.dotfiles/{}
+fi
+
+echo ".dotfiles">>.gitignore
+gdfiles config --local status.showUntrackedFiles no
+
+echo "...dotfiles clonados desde github!"
+sleep 1
+
 ############# instalar Oh My ZSH!
 
-echo "...Instalando oh-my-zsh:"
+echo "...descargando instalador de oh_my_zsh!:"
+sleep 1
 
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh --output-document=ohmyzsh_install.sh
 
+echo "...configurando oh_my_zsh! para usar .zshrc propio:"
+
+sleep 2
+
+sed -i 's/RUNZSH=${RUNZSH:-yes}/RUNZSH=${RUNZSH:-no}/g' ohmyzsh_install.sh
+sed -i 's/CHSH=${CHSH:-yes}/CHSH=${CHSH:-no}/g' ohmyzsh_install.sh
+sed -i 's/KEEP_ZSHRC=${KEEP_ZSHRC:-no}/KEEP_ZSHRC=${KEEP_ZSHRC:-yes}/g' ohmyzsh_install.sh
+
+echo "...instalando oh_my_zsh!:"
+
+sleep 2
+
+sh ohmyzsh_install.sh
+
+rm ohmyzsh_install.sh
 
 ############ activar servicios systemd
 
 echo "...Activando servicios:"
-
-sudo systemctl enable lightdm.service NetwokManager
+sleep 1
 
 ## descomentar la sig. linea si se instala en virtualbox SO invitado
-#sudo systemctl enable vboxservice.service
+sudo systemctl enable vboxservice.service &&\
+sudo systemctl start vboxservice.service
 
-
-############# copia de seguridad de dotfiles y evitar conflictos con 'git checkout'
-
-echo "...Haciendo copia de seguridad de .bashrc y .zshrc:"
-
-mkdir .backup
-mv .zshrc .backup
-mv .bashrc .backup
-
-
-############# clonar  dotfiles  ##################
-
-echo "...Clonando dotfiles desde github:"
-
-alias gdfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-
-echo ".dotfiles">>.gitignore
-
-git clone --bare https://github.com/daniel-testa/dotfiles.git $HOME/.dotfiles
-
-gdfiles checkout
-
-gdfiles config --local status.showUntrackedFiles no
+sudo systemctl enable NetwokManager.service
+sudo systemctl enable lightdm.service
 
 ############ reiniciar en login manager
+
+echo "...reiniciando"
+sleep 2
 
 reboot
